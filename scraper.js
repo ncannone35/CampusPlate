9da4cd5041f4fdb68e91eed41049934815885d89
Sam DeLong <sam@samdelong.com>
2023-11-03 19:24:00 -0400

    added web scraper

---
+++ scraper.js
@@ -0,0 +1,107 @@
+const request = require('sync-request')
+const fs = require('fs').promises
+const cheerio = require('cheerio')
+let recipeUrl = 'https://dining.rochester.edu/wp-content/themes/nmc_dining/ajax-content/recipe.php?recipe='
+let mealsUrl = 'https://dining.rochester.edu/locations/douglass-dining/?date='
+
+function currDateString() {
+
+    return '2023-11-03'
+
+}
+
+generateNutritionFile('nutrition.json')
+
+async function generateNutritionFile(path) {
+    console.log("Generating nutrition file...this will take a minute")
+    await fs.writeFile(path, JSON.stringify(getMealsByTime()))
+    console.log("All done")
+}
+
+async function readNutritionFile(path) {
+    let data = await fs.readFile(path, 'binary')
+    return Buffer.from(data).toString()
+}
+
+function getMealsByTime() {
+    let url = mealsUrl + currDateString()
+    let res = request('GET', url)
+    let data = res.body.toString()
+    const $ = cheerio.load(data)
+
+    let times = []
+    $('#menu-tabs').find('.c-tabs-nav__link-inner').each((i, elm) => {
+        times.push({
+            label: $(elm).text(),
+            places: []
+        })
+    })
+
+    $('#menu-tabs').find('.c-tab').each((i, elm) => {
+        let $n = $(elm)
+        $n.find('[id^="menu-station-data"]').each((j, elm2) => {
+            let $b = $(elm2)
+
+            let nn = $b.children(":first").children(":first").text();
+            times[i].places.push({ name: '', meals: [] })
+            $b.find('a.show-nutrition').each((k, elm3) => {
+                let $c = $(elm3)
+                times[i].places[j].meals.push({
+                    name: $c.text(),
+                    id: $c.attr('data-recipe'),
+                    info: getRecipeAtIndex($c.attr('data-recipe'))
+                })
+            })
+            /*console.log(times[i])
+            for(let place of times[i].places){
+              console.log(place)
+            }
+            */
+        })
+        $n.find('.toggle-menu-station-data').each((k, em) => {
+            times[i].places[k].name = $(em).text()
+        })
+
+    })
+
+    return times
+
+
+}
+
+
+function getRecipeAtIndex(i) {
+
+    let res = request('POST', `${recipeUrl}${i}`)
+    let data = JSON.parse(res.getBody('utf8'))
+    if (!data.success) return {}
+    //we don't like whitespace in this household
+    data.html = data.html.replace(/\s/g, '')
+
+
+    //fuck html, lets just hope that the responses are consistent (fingers crossed emoji)
+    let name = data.html.split('<h2>')[1].split('</h2>')[0]
+    let protein = data.html.split('Protein</b>')[1].split('</th>')[0]
+    let cals = data.html.split('Calories')[1]
+    cals = cals.split('</th>')[0]
+    cals = cals.slice(4)
+
+
+    let recipe = {
+        name: name,
+        protein: protein,
+        calories: parseInt(cals)
+
+    }
+
+    return recipe
+
+}
+
+module.exports = {
+
+    getRecipeAtIndex,
+    generateNutritionFile,
+    readNutritionFile
+
+}
