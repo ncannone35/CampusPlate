5d8e022378e9fecde68da49759de8abbc4b1752e
Justin Tsai <justintsai89@gmail.com>
2023-11-04 14:58:51 -0400

    index changes

--- campus-plate/src/pages/index.js
+++ campus-plate/src/pages/index.js
@@ -15,21 +15,7 @@ export default function Home() {
         <title>CampusPlate</title>
       </Head>
       <NavBar />
-      <BackgroundVideo /> {/* This will render the video as a background */}
-      <main className="flex flex-col min-h-screen w-full">
-        <div
-          id="homePageContentContainer"
-          className="flex flex-col h-[2000px] w-full bg-transparent" // Ensure the background is transparent
-        >
-          <div id="introSection">
-            {/* Your intro content here */}
-          </div>
-
-          <div id="moreLandingPageStuff">
-            {/* More content here */}
-          </div>
-        </div>
-      </main>
+      <BackgroundVideo /> 
     </>
   );
 }
