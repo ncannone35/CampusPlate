44562a0e5dc39097cb21abfd5334aa2fac89e6d8
Sifeng Chen <sifengchen2002@gmail.com>
2023-11-05 01:13:06 -0500

    final

--- campus-plate/src/pages/index.js
+++ campus-plate/src/pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <Head>
         {" "}
         <title>CampusPlate</title>
+        <link rel="icon" href="/chef.ico" />
       </Head>
 
       <div className="relative">
@@ -20,8 +21,12 @@ export default function Home() {
         <div className="absolute border- w-full min-h-screen flex items- justify-center z-0 ">
           <div className="text-white flex flex-col text-center space-y-[20px] mt-[35vh] sm:mt-[28vh] px-[20px]">
             {" "}
-            <h1 className="text-white text-4xl sm:text-6xl font-bold">This is CampusPlate </h1>
-            <h1 className="text-white text-3xl sm:text-4xl font-semibold">Let us help you decide what to eat </h1>
+            <h1 className="text-white text-4xl sm:text-6xl font-bold">
+              This is CampusPlate{" "}
+            </h1>
+            <h1 className="text-white text-3xl sm:text-4xl font-semibold">
+              Let us help you decide what to eat{" "}
+            </h1>
           </div>
         </div>
       </div>
